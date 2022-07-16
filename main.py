3542b304b626cb55dd335cf0b20a90b99d1acca7
Pedro Henrique <67080125+pedro-brandao@users.noreply.github.com>
2022-07-16 19:05:48 +0000

    comments referent the  function of code

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # Minha primeira ferramenta de pentest construida no curso de pentest
 
-import socket
+import socket # importa a biblioteca SOCKET
 
-host = input(str("Digite o host que quer fazer o scan!:"))
+host = input(str("Digite o host que quer fazer o scan!:")) #solicita entrada do site ou IP do host
 
-ports = [80, 443, 1443, 21, 22, 3306, 25]
+ports = [80, 443, 1443, 21, 22, 3306, 25] # portas escaneadas
 
 for port in ports:
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.settimeout(0.1)
+	client.settimeout(0.1) # seta tempo de espera pelo programa
 	code = client.connect_ex((host ,port))
 	print(code)
 	if code == 0:
-		print(port, "OPEN")
+		print(port, "OPEN") # printa na tela o numero da porta e OPEN caso esteja aberta ou fechada
 
 
 #response = client.recv(1024)
